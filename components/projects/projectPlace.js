0090384dd590c7a4cfb3dce26018923715a2d1bd
gabypega1997 <gabypega1997@gmail.com>
2022-01-18 18:09:08 +0100

    add carousel

--- components/projects/projectPlace.js
+++ components/projects/projectPlace.js
@@ -3,26 +3,44 @@ import classes from "./projectPlace.module.css"
 import {FaAngleLeft,FaAngleRight} from "react-icons/fa"
 import { IoLogoJavascript,IoLogoHtml5 } from "react-icons/io";
 import Image from "next/image"
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel'; 
 
+const projects =[
+     {
+    id : 1,
+    img: "/minion.jpg",
+    name : "Message",
+    desc : "This project i maked 2 years ago with vanila js",
+    date : "29.06.2020",
+    link : "https://message-ten-lake.vercel.app",
+    tehnologies: "Js HTML Css"
+    
+},
+{
+    id : 2,
+    img:"/minion.jpg",
+    name : "Movie Fighter",
+    desc : "With this project i learn a lot about how api's works. And i understand the power of vanila js and css",
+    date : "04.11.2019",
+    link : "https://github.com/gabypega1997/Movie-Fighter",
+    tehnologies: "Js HTML Css ApiOmdb"
+}
+]
 function ProjectPlace (){
     
     return (
-        <div className={classes.main} >
-            <FaAngleLeft className={classes.projectControler}/>
-            <div className={classes.projectImage}>
-                <Image className={classes.image} src="/minion.jpg" alt="minion" layout='fill'/>
-                <div className={classes.projectDesc}>
-                    <h1 className={classes.projectDesc_title} >Project 1</h1>
-                    <h4>For this project I used vanilla Javascrit to manipulate the forms and to show differents bettwen winner and loser</h4>
-                    <p className={classes.projectDesc_date}>24.11.2021</p>
-                    <div className={classes.projectDesc_logoTehnUsed}>
-                        <IoLogoJavascript/>
-                        <IoLogoHtml5/>
+        < div className={classes.main} >
+            <Carousel>
+                {projects.map(project =>(
+                
+                    <div>
+                        <img src={project.img} alt={project.name}></img>
+                        <p className='legende'>{project.desc}</p>
                     </div>
+            ))}
+            </Carousel>
 
-                </div>
-            </div>
-            <FaAngleRight className={classes.projectControler}/>
         </div>
     )
 }
