5b77e85ac30a1dc2044b3d386c5b112199853476
gabypega1997 <gabypega1997@gmail.com>
2022-01-22 14:15:15 +0100

    add carrousel future

--- components/projects/projectPlace.js
+++ components/projects/projectPlace.js
@@ -7,7 +7,7 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel'; 
 
 const projects =[
-     {
+        {
     id : 1,
     img: "/minion.jpg",
     name : "Message",
@@ -25,6 +25,15 @@ const projects =[
     date : "04.11.2019",
     link : "https://github.com/gabypega1997/Movie-Fighter",
     tehnologies: "Js HTML Css ApiOmdb"
+},
+{
+    id : 3,
+    img:"/minion.jpg",
+    name : "Movie Fighter",
+    desc : "With this project i learn a lot about how api's works. And i understand the power of vanila js and css",
+    date : "04.11.2019",
+    link : "https://github.com/gabypega1997/Movie-Fighter",
+    tehnologies: "Js HTML Css ApiOmdb"
 }
 ]
 function ProjectPlace (){
@@ -33,10 +42,27 @@ function ProjectPlace (){
         < div className={classes.main} >
             <Carousel>
                 {projects.map(project =>(
-                
-                    <div>
-                        <img src={project.img} alt={project.name}></img>
-                        <p className='legende'>{project.desc}</p>
+                <div key={project}>
+                    <Image
+                        src={project.img}
+                        alt={project.name}
+                        width={900}
+                        height={620}    
+                        className={classes.Image}                 
+                    ></Image>
+                        <h2 className={classes.carouselTitle}>{project.name}</h2>
+                        <div className={classes.container_carrousel}>
+                            <div className={classes.desc}>
+                                <p className='legende' >{project.desc}</p>
+                            </div>
+                            <div className={classes.utilities}>
+                                <a className={classes.hoverLink} href={project.link}><b>Link: </b>{project.link}</a>
+                                <p><b>Tehnologies: </b>{project.tehnologies}</p>
+                                <p>{project.date}</p>
+                            </div>
+                        </div>                    
+                        <br/>
+                        
                     </div>
             ))}
             </Carousel>
